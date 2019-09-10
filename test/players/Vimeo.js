4950cc10448445d620d117a741b18dd4b05418d9
Blaine OBrien <blaineo@gmail.com>
2019-09-10 11:24:21 -0700

    chore( removing background color from test )

--- test/players/Vimeo.js
+++ test/players/Vimeo.js
@@ -65,7 +65,7 @@ test('render()', t => {
   const style = {
     width: '100%',
     height: '100%',
-    overflow: 'hidden'
+    overflow: 'hidden',
     display: undefined
   }
   t.true(wrapper.contains(
